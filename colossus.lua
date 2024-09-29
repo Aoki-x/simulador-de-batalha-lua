145ae619a6e46331dd3864ee1fafb7c424d4f14a
Aoki-x <72118930+Aoki-x@users.noreply.github.com>
2024-09-29 10:13:59 -0300

    adicionando criaturas e suas definições

---
+++ colossus.lua
@@ -0,0 +1,14 @@
+local colossus = {}
+
+colossus.name = "Crimsom Colossus"
+colossus.description = "Um monstro terrível e muito resistente."
+
+colossus.maxHealth = 50
+colossus.health = 50
+colossus.attack = 5
+colossus.defense = 6
+colossus.speed = 0
+
+
+
+return colossus
